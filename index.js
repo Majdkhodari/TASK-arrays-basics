f575b67479b295ace3c99325c699c06e4702eee5
Majdkhodari <majdkhudary@gmail.com>
2022-01-10 16:01:04 +0200

    Arrays Task Done

---
+++ index.js
@@ -0,0 +1,14 @@
+const emptyArray = []
+let hobbies = ["basketBall", "Swimming", "Reading", "Coding", "Cars", "Badminton"]
+const numbers = [0, 1, 2, 3, 4, 5]
+
+console.log(numbers[1])
+console.log (hobbies[5])
+hobbies.push ("fake")
+console.log (hobbies[1])
+console.log(hobbies.length)
+console.log(hobbies.pop (5, 6))
+console.log (hobbies)
+
+hobbies = []
+console.log (hobbies)
